b4f28d7fd5b75f72fea4b6227714122166a1bddd
Cafe <dachscafe@yahoo.co.jp>
2023-02-24 20:42:57 +0900

    Initial Commit

---
+++ SelectTimeSample/SelectTimeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelectTimeSampleApp.swift
+//  SelectTimeSample
+//
+//  Created by Cafe on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SelectTimeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
